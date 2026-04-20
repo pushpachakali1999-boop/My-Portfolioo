30674f381cd6b844bac2c3e6dea012a3413ddcf2
Pushpa Chakali <pushpa@example.com>
2026-04-20 14:25:17 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// Smooth scrolling
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", function(e) {
+// function toggleMenu(){
+//     document.getElementById("navLinks").classList.toggle("active");
+// }
+
+// document.querySelectorAll("nav a").forEach(link=>{
+//     link.addEventListener("click",function(e){
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute("href"))
+//         .scrollIntoView({behavior:"smooth"});
+//     })
+// })<script>
+// function showMessage() {
+//     document.getElementById("msg").innerHTML = "Message sent successfully!";
+      
+// }
+
+// </script>
+
+
+function toggleMenu(){
+    document.getElementById("navLinks").classList.toggle("show");
+}
+
+// Smooth scroll
+document.querySelectorAll("nav a").forEach(link=>{
+    link.addEventListener("click",function(e){
         e.preventDefault();
         document.querySelector(this.getAttribute("href"))
-            .scrollIntoView({ behavior: "smooth" });
+        .scrollIntoView({behavior:"smooth"});
     });
 });
